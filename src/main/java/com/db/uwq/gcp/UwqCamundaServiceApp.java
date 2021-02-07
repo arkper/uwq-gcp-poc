117dbbce863fc61c186606c91e774397b7e4f859
arkper <arkper@gmail.com>
2021-02-07 13:54:00 -0500

    Added code

---
+++ src/main/java/com/db/uwq/gcp/UwqCamundaServiceApp.java
@@ -0,0 +1,12 @@
+package com.db.uwq.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwqCamundaServiceApp {
+	public static void main(String... args) {
+		SpringApplication.run(UwqCamundaServiceApp.class, args);
+	}
+
+}
